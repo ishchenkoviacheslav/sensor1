b34c5c068e0b5da59967d63cc9c349c71cf62951
user <user@DESKTOP-05J4HLG>
2016-08-04 17:03:38 +0200

    по Х работает норм. нужно думать логику сравнения

---
+++ sensor1/AccelXYZ.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sensor1
+{
+    class AccelXYZ
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", X, Y, Z);
+        }
+    }
+}
--- sensor1/MainPage.xaml.cs
+++ sensor1/MainPage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using Windows.Devices.Sensors;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace sensor1
@@ -22,9 +22,85 @@ namespace sensor1
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Accelerometer _accelerometer;
+        List<AccelXYZ> listXYZ = null;
+        List<AccelXYZ> listTop = null;
         public MainPage()
         {
             this.InitializeComponent();
+            listXYZ = new List<AccelXYZ>();
+            _accelerometer = Accelerometer.GetDefault();
+            
+            if(_accelerometer == null)
+            {
+                textBox.Text = "kein accelerometer";
+            }
+        }
+
+        private async void _accelerometer_ReadingChanged(Accelerometer sender, AccelerometerReadingChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                listXYZ.Add(new AccelXYZ() { X = args.Reading.AccelerationX, Y = args.Reading.AccelerationY, Z = args.Reading.AccelerationZ });
+                //textBox.Text = string.Format("X: {0,5:0.00} Y: {1,5:0.00} Z: {2,5:0.00}", args.Reading.AccelerationX, args.Reading.AccelerationY, args.Reading.AccelerationZ);
+            });
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_accelerometer != null)
+            {
+                _accelerometer.ReadingChanged += _accelerometer_ReadingChanged;
+            }
+            textBox.Text = "";
+            textBoxTop.Text = "";
+            listXYZ = new List<AccelXYZ>();
+            listTop = new List<AccelXYZ>();
+        }
+
+        private void stopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_accelerometer != null)
+            {
+                _accelerometer.ReadingChanged -= _accelerometer_ReadingChanged;
+            }
+        }
+
+        private void ShowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AccelXYZ[] arrayXYZ = listXYZ.ToArray();
+            foreach (AccelXYZ item in listXYZ)
+            {
+                textBox.Text += item.X;
+                //textBox.Text += item.Y;
+                //textBox.Text += item.Z;
+                textBox.Text += "\n";
+            }
+            if(arrayXYZ[0].X > arrayXYZ[1].X)
+            {
+                listTop.Add(arrayXYZ[0]);
+            }
+            for (int i = 0; i < arrayXYZ.Length; i++)
+            {
+                if(((i+1) < arrayXYZ.Length) && ((i - 1) >= 0))
+                {
+                    if ((arrayXYZ[i-1].X < arrayXYZ[i].X) &&(arrayXYZ[i].X > arrayXYZ[i+1].X))
+                    {
+                        listTop.Add(arrayXYZ[i]);
+                    }
+                }
+            }
+            if ()
+            {
+
+            }
+            foreach (AccelXYZ item in listTop)
+            {
+                textBoxTop.Text += item.X;
+                //textBox.Text += item.Y;
+                //textBox.Text += item.Z;
+                textBoxTop.Text += "\n";
+            }
         }
     }
 }
