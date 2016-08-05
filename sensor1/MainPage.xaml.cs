dac676f59439eaed45fb183a2971207eff75cefa
user <user@DESKTOP-05J4HLG>
2016-08-06 00:38:49 +0200

    все вроде норм. нужно тестить

--- sensor1/MainPage.xaml.cs
+++ sensor1/MainPage.xaml.cs
@@ -24,7 +24,7 @@ namespace sensor1
     {
         private Accelerometer _accelerometer;
         List<AccelXYZ> listXYZ = null;
-        List<AccelXYZ> listTop = null;
+        List<AccelXYZ> listTopBot = null;
         public MainPage()
         {
             this.InitializeComponent();
@@ -55,7 +55,7 @@ namespace sensor1
             textBox.Text = "";
             textBoxTop.Text = "";
             listXYZ = new List<AccelXYZ>();
-            listTop = new List<AccelXYZ>();
+            listTopBot = new List<AccelXYZ>();
         }
 
         private void stopBtn_Click(object sender, RoutedEventArgs e)
@@ -69,32 +69,30 @@ namespace sensor1
         private void ShowBtn_Click(object sender, RoutedEventArgs e)
         {
             AccelXYZ[] arrayXYZ = listXYZ.ToArray();
-            foreach (AccelXYZ item in listXYZ)
+            foreach (AccelXYZ item in listXYZ) // вывод в текстбокс всего
             {
                 textBox.Text += item.X;
                 //textBox.Text += item.Y;
                 //textBox.Text += item.Z;
                 textBox.Text += "\n";
             }
-            if(arrayXYZ[0].X > arrayXYZ[1].X)//добавление первого если он больше чем второй
-            {
-                listTop.Add(arrayXYZ[0]);
-            }
+            listTopBot.Add(arrayXYZ[0]);//всегда добавляем первый и последний
             for (int i = 0; i < arrayXYZ.Length; i++)
             {
-                if(((i+1) < arrayXYZ.Length) && ((i - 1) >= 0))
+                if(((i+1) < arrayXYZ.Length) && ((i - 1) >= 0)) // проверка что мы в пределах массива. Чтобы работать ТОЛЬКо от 0(включительно) и до -1 от конца.
+                    //например если массив из 10 тоесть от 0 до 9 то мы попадем в иф ТОЛЬКо начиная с первого (индекс 1) сдвига и до предпоследнего (длинна в ел. -2)
                 {
-                    if ((arrayXYZ[i-1].X < arrayXYZ[i].X) &&(arrayXYZ[i].X > arrayXYZ[i+1].X))
+                    if ( 
+                        ((arrayXYZ[i-1].X <= arrayXYZ[i].X) &&(arrayXYZ[i].X >= arrayXYZ[i+1].X)) // если мы нашли самую высокую вершину
+                        || ((arrayXYZ[i-1].X >= arrayXYZ[i].X) && (arrayXYZ[i].X <= arrayXYZ[i+1].X))//если мы нашли самую низкую "вершину"
+                        )
                     {
-                        listTop.Add(arrayXYZ[i]);
+                        listTopBot.Add(arrayXYZ[i]);
                     }
                 }
             }
-            if (arrayXYZ[arrayXYZ.Length-1].X > arrayXYZ[arrayXYZ.Length - 2].X) // добавление последнего если он больше чем предпоследний
-            {
-                listTop.Add(arrayXYZ[arrayXYZ.Length - 1]);
-            }
-            foreach (AccelXYZ item in listTop)
+            listTopBot.Add(arrayXYZ[arrayXYZ.Length - 1]);//всегда добавляем первый и последний
+            foreach (AccelXYZ item in listTopBot) // вывод в текстбокс только вершин - низин
             {
                 textBoxTop.Text += item.X;
                 //textBox.Text += item.Y;
